1b44b59520c4c56b7514e6be9fc71c0c172ec8d6
Dm_Grt <43096165+DmGrt@users.noreply.github.com>
2020-07-29 14:04:51 +0300

    Update Palindrome.java

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -4,6 +4,5 @@ public class Palindrome {
     public boolean isPalindrome(String text) {
         text = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         return text.equals(new StringBuilder(text).reverse().toString());
-
     }
 }
